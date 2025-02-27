5fe6508efc76803f4b04686bdb7c6a63b8ac5602
EdisonValdez <eddynvg@hotmail.com>
2025-02-27 19:19:09 +0100

    Fixing errors in provessors iien

--- lscms/lscms/settings/base.py
+++ lscms/lscms/settings/base.py
@@ -66,30 +66,29 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "lscms.urls"
- 
+
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            os.path.join(PROJECT_DIR, "templates"),
         ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.i18',
-                'wagtail.contrib.settings.context_processors.settings'
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.i18n",  
+                "wagtail.contrib.settings.context_processors.settings",
             ],
         },
     },
 ]
  
 WSGI_APPLICATION = "lscms.wsgi.application"
-
-
+ 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
